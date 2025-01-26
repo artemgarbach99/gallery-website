87439a8fc6093c77f646a4c0c882a7375d7367d2
Артем <artem@MacBook-Air-Artem.local>
2025-01-26 13:11:46 +0300

    добавление к поиску not found

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -14,8 +14,6 @@ export const Search = () => {
 		card => searchTerm && card.alt_description.toLowerCase().includes(searchTerm.toLowerCase())
 	)
 
-	console.log(filteredCards)
-
 	return (
 		<div className={search.body}>
 			<div className={search.search}>
@@ -30,6 +28,30 @@ export const Search = () => {
 				</button>
 			</div>
 			{searchTerm && (
+        filteredCards.length > 0 ? (
+          <div className={search.dropdown}>
+            {filteredCards.map(card => (
+              <Link to={`/card/${card.id}`} key={card.id} className={search.card}>
+                <div className={search.image}>
+                  <img src={card.urls.thumb} alt='' />
+                </div>
+                <div className={search.content}>
+                  <div className={search.title}>{card.alt_description}</div>
+                  <div className={search.user}>
+                    <FaUserNinja color='#f81539' size={16} />
+                    {card.user.username}
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <div className={search.dropdown}>
+						<div className={search.title}>not found!</div>
+          </div>
+        )
+      )}
+			{/* {searchTerm && filteredCards.length > 0 ? (
 				<div className={search.dropdown}>
 					{filteredCards.map(card => (
 						<Link to={`/card/${card.id}`} key={card.id} className={search.card}>
@@ -46,7 +68,7 @@ export const Search = () => {
 						</Link>
 					))}
 				</div>
-			)}
+			) : ('123')} */}
 		</div>
 	)
 }

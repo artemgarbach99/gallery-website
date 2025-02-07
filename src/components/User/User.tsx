c50be92129a022e630137e0329c0d34bf58c7bb3
Artem <artemgarbach99@gmail.com>
2025-02-07 12:18:24 +0300

    dropdown меню для user в header

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -5,21 +5,42 @@ import user from '@components/User/User.module.scss'
 import { FaUserAlt } from 'react-icons/fa'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { IoIosArrowDown } from 'react-icons/io'
+import { RiLogoutBoxRLine } from 'react-icons/ri'
+import { useState } from 'react'
 
 export const User = () => {
 	const dispatch: AppDispatch = useDispatch()
 	const { isAuth, email } = useAuth()
+
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+	const dropDownHandler = () => {
+		setIsMenuOpen(prevState => !prevState)
+	}
+
 	return (
 		<div className={user.user}>
 			<div className={user.avatar}>
 				<FaUserAlt size={24} />
 				{/* <img src='/user.png' alt='' /> */}
 			</div>
-			{/* <div className={user.name}>Behzad</div> */}
 			{isAuth ? (
 				<div className={user.links}>
 					<div className={user.name}>{email}</div>
-					<button onClick={() => dispatch(userActions.removeUser())}>log out</button>
+					<button
+						type='button'
+						className={isMenuOpen ? `${user.button} ${user.open}` : user.button}
+						onClick={dropDownHandler}>
+						<IoIosArrowDown />
+					</button>
+					{isMenuOpen && (
+						<div className={user.dropdown}>
+							<button className={user.logout} onClick={() => dispatch(userActions.removeUser())}>
+								<RiLogoutBoxRLine size={16} /> log out
+							</button>
+						</div>
+					)}
 				</div>
 			) : (
 				<div className={user.links}>

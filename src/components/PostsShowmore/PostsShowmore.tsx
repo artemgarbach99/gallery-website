ec879a8678ae0014b27404b84b549614be01a41a
Artem <artemgarbach99@gmail.com>
2025-03-14 18:54:00 +0300

    Блок с постами с подгрузкой по кнопке

---
+++ src/components/PostsShowmore/PostsShowmore.tsx
@@ -0,0 +1,37 @@
+import { RootState } from '@/store/store'
+import { useSelector } from 'react-redux'
+import { SliderPostsCard } from '../SliderPostsCard/SliderPostsCard'
+import global from '@assets/styles/global.module.scss'
+import posts from '@components/Posts/Posts.module.scss'
+import styles from '@components/PostsShowmore/PostsShowmore.module.scss'
+import { useState } from 'react'
+
+export const PostsShowmore = () => {
+	const { images } = useSelector((state: RootState) => state.images)
+
+	const [visibleItems, setVisibleItems] = useState(8)
+	console.log(visibleItems)
+
+	const loadMore = () => {
+		setVisibleItems(prevItems => prevItems + 8)
+	}
+
+	return (
+		<div className={posts.wrap}>
+			<div className={global.h4}>All Posts</div>
+			<div className={styles.list}>
+				{images
+					.slice(0, visibleItems)
+					// .filter(card => card.likes >= 50)
+					.map((card, index) => (
+						<SliderPostsCard key={index} card={card} />
+					))}
+			</div>
+			<div className={styles.bottom}>
+				<button onClick={loadMore} className={styles.button}>
+					showmore
+				</button>
+			</div>
+		</div>
+	)
+}
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,6 +2,7 @@ import { Posts } from '@/components/Posts/Posts'
 import { TopSectionCards } from '@/components/TopSectionCards/TopSectionCards'
 import { Slider } from '@/components/Slider/Slider'
 import main from '@pages/Main/Main.module.scss'
+import { PostsShowmore } from '@/components/PostsShowmore/PostsShowmore'
 // import { useDispatch } from 'react-redux'
 // import { useAuth } from '@/hooks/useAuth'
 // import { Navigate } from 'react-router-dom'
@@ -36,6 +37,7 @@ export const Main = () => {
 					<Slider />
 				</div>
 				<Posts />
+				<PostsShowmore />
 			</div>
 		</div>
 	)
